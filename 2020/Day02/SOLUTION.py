27a2c32884010882b6f5074e28b9cbdf49ffe7fb
MartinTam <hongvtam@protonmail.com>
2021-03-02 12:13:13 +0100

    Part one done

--- 2020/Day02/SOLUTION.py
+++ 2020/Day02/SOLUTION.py
@@ -8,7 +8,7 @@ for x in line:
     lines.append(x)
 
 
-def numberIntoDigit(listOfDigigt):
+def digitsIntoOneNumber(listOfDigigt):
     number = 0
     if len(listOfDigigt) == 2:
         number = (listOfDigigt[0] *10) + listOfDigigt[1]
@@ -16,9 +16,28 @@ def numberIntoDigit(listOfDigigt):
         number = listOfDigigt[0]
     return number
 
-def createListOfTuples(lines):
+def checkCorrectPassword(oneLineTuple):
+    minimum = oneLineTuple[0]
+    maximum = oneLineTuple[1]
+    letter = oneLineTuple[2]
+    password = oneLineTuple[3]
+    count = 0
+    correctPassword = False
+
+    for x in range(len(password)):
+        if password[x] == letter:
+            count += 1
+    
+    if minimum <= count <= maximum:
+        correctPassword = True
+    
+    return correctPassword
+
+
+def countCorrectPassword(lines):
 
     listOfTuples = []
+    correct = 0
 
     for x in lines:
         firstNumber = []
@@ -27,6 +46,8 @@ def createListOfTuples(lines):
         password = []
         rememberMinusSign = 0
         rememberSpace = 0
+
+
         for i in range(3):
             if x[i] != '-':
                 firstNumber.append(x[i])
@@ -45,11 +66,11 @@ def createListOfTuples(lines):
         for i in range(rememberSpace + 4, len(x)-1):
             password.append(x[i])
 	
-        oneLineTuple = (numberIntoDigit( [int(x) for x in firstNumber] ), numberIntoDigit( [int(x) for x in secondNumber] ), [str(x) for x in letter][0], [str(x) for x in password])
-        listOfTuples.append(oneLineTuple)
+        oneLineTuple = (digitsIntoOneNumber( [int(x) for x in firstNumber] ), digitsIntoOneNumber( [int(x) for x in secondNumber] ), [str(x) for x in letter][0], [str(x) for x in password])
+        
+        if checkCorrectPassword(oneLineTuple):
+            correct += 1
     
-    return listOfTuples
+    return correct
 
-
-
-createListOfTuples(lines)
+print(f"Part 1: ",countCorrectPassword(lines))
